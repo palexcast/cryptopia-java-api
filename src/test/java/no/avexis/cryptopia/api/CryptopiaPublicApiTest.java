34c73414c5277c9c01e5ebf1b6602e8b0fc9cf03
Alexander Castillo <alexander_castillo@bitbucket.org>
2017-08-13 19:40:18 +0200

    Update test names

--- src/test/java/no/avexis/cryptopia/api/CryptopiaPublicApiTest.java
+++ src/test/java/no/avexis/cryptopia/api/CryptopiaPublicApiTest.java
@@ -4,6 +4,8 @@ package no.avexis.cryptopia.api;
 import no.avexis.cryptopia.exceptions.MissingParameterException;
 import no.avexis.cryptopia.models.pub.Currency;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -22,140 +24,155 @@ class CryptopiaPublicApiTest {
     }
 
     @Test
-    void getCurrencies() throws Exception {
+    void Valid_GetCurrencies() throws Exception {
         List<Currency> currencyList = api.getCurrencies();
         Currency cur = currencyList.get(0);
         assertFalse(cur.getName().isEmpty());
     }
 
     @Test
-    void getTradePairs() throws Exception {
+    void Valid_GetTradePairs() throws Exception {
         api.getTradePairs();
     }
 
-    @Test
-    void getMarkets_ValidBothHaveValues() throws Exception {
-        api.getMarkets("baseMarket", 24);
+    @Nested
+    @DisplayName("Get Markets")
+    class getMarkets {
+        @Test
+        void Valid_NoNullValues() throws Exception {
+            api.getMarkets("baseMarket", 24);
+        }
+
+        @Test
+        void Valid_BothNull() throws Exception {
+            api.getMarkets(null, null);
+        }
+
+        @Test
+        void Valid_BaseMarketIsNull() throws Exception {
+            api.getMarkets(null, 24);
+        }
+
+        @Test
+        void Valid_HoursIsNull() throws Exception {
+            api.getMarkets("baseMarket", null);
+        }
     }
 
-    @Test
-    void getMarkets_ValidBothNull() throws Exception {
-        api.getMarkets(null, null);
+    @Nested
+    @DisplayName("Get Market")
+    class getMarket {
+        @Test
+        void Valid() throws Exception {
+            api.getMarket("nameOrId", null);
+            api.getMarket("nameOrId", 24);
+        }
+
+        @Test
+        void MissingParameterException_BothAreNull() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarket(null, null));
+        }
+
+        @Test
+        void MissingParameterException_NameOrIdIsNull() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarket(null, 24));
+        }
     }
 
-    @Test
-    void getMarkets_ValidBaseMarketNull() throws Exception {
-        api.getMarkets(null, 24);
+
+    @Nested
+    @DisplayName("Get Market History")
+    class getMarketHistory {
+        @Test
+        void Valid_NoNullValues() throws Exception {
+            api.getMarketHistory("nameOrId", 24);
+        }
+
+        @Test
+        void Valid_HoursIsNull() throws Exception {
+            api.getMarketHistory("nameOrId", null);
+        }
+
+        @Test
+        void MissingParameterException_BothAreNull() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarketHistory(null, null));
+            assertEquals(NAME_OR_IS_REQ, exception.getMessage());
+        }
+
+        @Test
+        void MissingParameterException_NameOrIdIsNull() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarketHistory(null, 24));
+            assertEquals(NAME_OR_IS_REQ, exception.getMessage());
+        }
     }
 
-    @Test
-    void getMarkets_ValidHoursNull() throws Exception {
-        api.getMarkets("baseMarket", null);
+
+    @Nested
+    @DisplayName("Get Market Orders")
+    class getMarketOrders {
+
+        @Test
+        void Valid_NoNullValues() throws Exception {
+            api.getMarketOrders("nameOrId", 24);
+        }
+
+        @Test
+        void Valid_CountIsNull() throws Exception {
+            api.getMarketOrders("nameOrId", null);
+        }
+
+        @Test
+        void MissingParameterException_BothAreNull() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarketOrders(null, null));
+            assertEquals(NAME_OR_IS_REQ, exception.getMessage());
+        }
+
+        @Test
+        void MissingParameterException_NameOrIdIsNull() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarketOrders(null, 24));
+            assertEquals(NAME_OR_IS_REQ, exception.getMessage());
+        }
     }
 
-    @Test
-    void getMarket_Valid() throws Exception {
-        api.getMarket("nameOrId", null);
-        api.getMarket("nameOrId", 24);
-    }
+    @Nested
+    @DisplayName("Get Market Groups")
+    class getMarketOrderGroups {
 
-    @Test
-    void getMarket_MissingParameterExceptionBothNull() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarket(null, null);
-        });
-    }
+        @Test
+        void Valid_NoNullValues() throws Exception {
+            api.getMarketOrderGroups(new String[]{"Markets"}, 24);
+        }
 
-    @Test
-    void getMarket_MissingParameterExceptionNameOrIdNull() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarket(null, 24);
-        });
-    }
+        @Test
+        void Valid_CountIsNull() throws Exception {
+            api.getMarketOrderGroups(new String[]{"Markets"}, null);
+        }
 
-    @Test
-    void getMarketHistory_ValidWithHours() throws Exception {
-        api.getMarketHistory("nameOrId", 24);
-    }
+        @Test
+        void MissingParameterException_BothValuesAreNull() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarketOrderGroups(null, null));
+            assertEquals(MARKETS_REQ, exception.getMessage());
+        }
 
-    @Test
-    void getMarketHistory_ValidNoHours() throws Exception {
-        api.getMarketHistory("nameOrId", null);
-    }
+        @Test
+        void MissingParameterException_MarketsEmptyArray() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarketOrderGroups(new String[0], 24));
+            assertEquals(MARKETS_REQ, exception.getMessage());
+        }
 
-    @Test
-    void getMarketHistory_MissingParameterExceptionBothNull() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarketHistory(null, null);
-        });
-        assertEquals(NAME_OR_IS_REQ, exception.getMessage());
-    }
-
-    @Test
-    void getMarketHistory_MissingParameterExceptionNameOrIdNull() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarketHistory(null, 24);
-        });
-        assertEquals(NAME_OR_IS_REQ, exception.getMessage());
-    }
-
-    @Test
-    void getMarketOrders_ValidWithCount() throws Exception {
-        api.getMarketOrders("nameOrId", 24);
-    }
-
-    @Test
-    void getMarketOrders_ValidNoCount() throws Exception {
-        api.getMarketOrders("nameOrId", null);
-    }
-
-    @Test
-    void getMarketOrders_ThrowsMissingParameterExceptionBothNull() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarketOrders(null, null);
-        });
-        assertEquals(NAME_OR_IS_REQ, exception.getMessage());
-    }
-
-    @Test
-    void getMarketOrders_MissingParameterExceptionNameOrIdNull() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarketOrders(null, 24);
-        });
-        assertEquals(NAME_OR_IS_REQ, exception.getMessage());
-    }
-
-    @Test
-    void getMarketOrderGroups_ValidWithCount() throws Exception {
-        api.getMarketOrderGroups(new String[]{"Markets"}, 24);
-    }
-
-    @Test
-    void getMarketOrderGroups_ValidNoCount() throws Exception {
-        api.getMarketOrderGroups(new String[]{"Markets"}, null);
-    }
-
-    @Test
-    void getMarketOrderGroups_ThrowsMissingParameterExceptionBothValuesNull() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarketOrderGroups(null, null);
-        });
-        assertEquals(MARKETS_REQ, exception.getMessage());
-    }
-
-    @Test
-    void getMarketOrderGroups_MissingParameterExceptionMarketsEmptyArray() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarketOrderGroups(new String[0], 24);
-        });
-        assertEquals(MARKETS_REQ, exception.getMessage());
-    }
-
-    @Test
-    void getMarketOrderGroups_MissingParameterExceptionMarketsNull() throws Exception {
-        Throwable exception = assertThrows(MissingParameterException.class, () -> {
-            api.getMarketOrderGroups(null, 24);
-        });
-        assertEquals(MARKETS_REQ, exception.getMessage());
+        @Test
+        void MissingParameterException_MarketsIsNull() throws Exception {
+            Throwable exception = assertThrows(MissingParameterException.class,
+                    () -> api.getMarketOrderGroups(null, 24));
+            assertEquals(MARKETS_REQ, exception.getMessage());
+        }
     }
 }
